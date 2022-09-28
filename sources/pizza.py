6dee9e7b3d6d6dd9bf08d93b389f01405bf01295
Lastef <stephanieb60@laposte.net>
2022-09-28 15:12:54 +0200

    New pizza

--- sources/pizza.py
+++ sources/pizza.py
@@ -35,5 +35,9 @@ RECIPES = {
     'mediterranea': {
         'ingredients': {'mozzarella': 3, 'pepper': 4, 'onion': 3},
         'price': 11,
+    },
+    'my pizza': {
+        'ingredients': {'garlic': 5, 'cheddar': 4, 'ham': 3},
+        'price': 11,
     }
 }
